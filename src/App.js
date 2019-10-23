e365fa1081a04b069d8f4485edcf88fa60910fb4
Adamski <Kamil.Adamski@unilever.com>
2019-10-23 10:36:34 +0200

    useState example + some function around

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import React,{useState} from 'react';
 function App() {
   const[userQuery,setUserQuery]=useState('');
 
+const updateUserQuery=event=>{
+  console.log('userQuery',userQuery)
+  setUserQuery(event.target.value)
+}
+
+const handleKeyPress=event =>{
+  if (event.key==='Enter'){
+    searchQuery();
+  }
+}
+
+const searchQuery=()=>{
+  window.open(`https://google.com/search?q=${userQuery}`,'_blank');
+}
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello Kamil</h1>
+      <div className='form'>
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+        <button onClick={searchQuery} >Search</button>
+      </div>
     </div>
   );
 }
